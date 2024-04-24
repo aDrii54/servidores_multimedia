f77ab194a319aec453c520746fa899894a653631
aDrii54 <79159428+aDrii54@users.noreply.github.com>
2024-04-24 21:48:25 +0200

    Add files via upload

---
+++ alerta_pelis.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+################################################
+## Fecha de creación - 15/02/2024             ## 
+## 					      ##
+## Alerta si hay pelis nuevas o eliminadas    ##
+##                                            ##
+################################################
+set -x
+cd $(dirname $0)
+source ./alerta_pelis_funciones.sh
+
+#Ruta donde guardes tus pelis
+RUTA_PELIS=/media/terramaster/plex/Peliculas
+
+#Siempre tiene que existir un old.temp, si no existe, lo crea.
+#En este old.temp guadaremos la lista de pelis con formato "Titulo", sin año y sustituiremos los espacios por "."
+if [[ ! -f old.temp ]]; then 
+	ls $RUTA_PELIS | grep -wo "^.*[0-9][0-9][0-9][0-9]" | sed 's/ /./g'| sed 's/(//g' > old.temp
+fi
+
+#Crearemos otro fichero llamada compara.temp para comparar la lista de peliculas de old.temp con compara.temp
+ls $RUTA_PELIS | grep -wo "^.*[0-9][0-9][0-9][0-9]" | sed 's/ /./g' | sed 's/(//g' > compara.temp
+
+#Aquí usaremos diff para sacar las diferencias entre ambos ficheros
+diff -y --suppress-common-lines 'old.temp' 'compara.temp' > cambios.temp
+
+#Si la salida es 0 no ha habido cambios. Si es 1, hay cambios.
+if [[ $? == 0 ]]; then
+	rm compara.temp cambios.temp 2>&1
+	exit 0
+
+#Si hay cambios, el if entra aquí y con awk enviará a subidas.temp todo lo que pille como nuevo ">" y a borradas.temp lo que pille como borrado "<"
+elif [[ $? == 1 ]]; then
+	awk '{if ($0 ~ />/) print $2}' cambios.temp > subidas.temp
+	awk '{if ($0 ~ /</) print $1}' cambios.temp > borradas.temp
+
+#Si hay contenido en subidas.temp, ejecutamos la función aviso_subidas
+	if [ -s "subidas.temp" ]; then
+		aviso_subidas
+	fi
+
+#Si hay contenido en subidas.temp, ejecutamos la función aviso_borradas
+	if [ -s "borradas.temp" ]; then
+		aviso_borradas
+	fi
+fi
+
+#Hacemos esto para que el fichero de compara ahora sea old.temp y pueda compararse de nuevo con el compara.temp que se vaya a crear en una nueva ejecución.
+mv compara.temp old.temp 
+
---
+++ alerta_pelis_funciones.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+function aviso_subidas(){
+        while read line
+        do
+                titulo=$(sed 's/\(.*\)\.\([0-9]\{4\}\)/\1/' subidas.temp| sed 's/\./%20/g')
+                year=$(sed 's/.*\.\([0-9]\{4\}\)/\1/' subidas.temp)
+                portada=$(curl "https://api.themoviedb.org/3/search/movie?api_key=8ae08e11c69497e279f94e747befdbd2&query=${titulo}&year=${year}&language=es-ES" | jq '.results[].poster_path | "https://image.tmdb.org/t/p/w500" + .')
+                portada_w=$(echo $portada|sed 's/"//g'| awk '{ split($0,a," "); print a[1]}' ); wget $portada_w
+                portada_envio=$(echo $portada_w| sed 's@https://image.tmdb.org/t/p/w500/@@g')
+                sinopsis=$(curl "https://api.themoviedb.org/3/search/movie?api_key=8ae08e11c69497e279f94e747befdbd2&query=${titulo}&year=${year}&language=es-ES" | jq '.results[].overview')
+                sinopsis_e=$(echo $sinopsis| awk '{ split($0,a,"\""); print a[2]}')
+                nota=$(curl "https://api.themoviedb.org/3/search/movie?api_key=8ae08e11c69497e279f94e747befdbd2&query=${titulo}&year=${year}&language=es-ES" | jq '.results[].vote_average')
+                nota_e=$(echo $nota| awk '{ split($0,a," "); print a[1]}')
+                titulo=$(echo $titulo| sed -e 's/%20/ /g')
+                TOKEN="6408734443:AAHRtxZDrKiYZFWvxPpXm1Y97EnKiPnSoGM"
+                CHAT_ID="-1002094716188"
+                MESSAGE="✅NUEVA PELICULA SUBIDA
+<b>Nota (TMDB): </b>$nota_e
+<b>Año: </b>$year
+<b>Título: </b>$titulo
+<b>Sinopsis: </b>$sinopsis_e"
+		IMAGE_PATH="$(pwd)/$portada_envio"
+                curl -X POST \
+                -H 'Content-Type: multipart/form-data' \
+                -F chat_id=$CHAT_ID \
+                -F photo=@$IMAGE_PATH \
+                -F caption="$MESSAGE" \
+                -F parse_mode="HTML" \
+                https://api.telegram.org/bot$TOKEN/sendPhoto
+        done < subidas.temp
+        rm -rf $portada_envio
+}
+
+
+function aviso_borradas(){
+	while read line
+        do
+                titulo=$(sed 's/\(.*\)\.\([0-9]\{4\}\)/\1/' borradas.temp| sed 's/\./%20/g')
+                year=$(sed 's/.*\.\([0-9]\{4\}\)/\1/' borradas.temp)
+                portada=$(curl "https://api.themoviedb.org/3/search/movie?api_key=8ae08e11c69497e279f94e747befdbd2&query=${titulo}&year=${year}&language=es-ES" | jq '.results[].poster_path | "https://image.tmdb.org/t/p/w500" + .')
+                portada_w=$(echo $portada|sed 's/"//g'| awk '{ split($0,a," "); print a[1]}' ); wget $portada_w
+                portada_envio=$(echo $portada_w| sed 's@https://image.tmdb.org/t/p/w500/@@g')
+                sinopsis=$(curl "https://api.themoviedb.org/3/search/movie?api_key=8ae08e11c69497e279f94e747befdbd2&query=${titulo}&year=${year}&language=es-ES" | jq '.results[].overview')
+                sinopsis_e=$(echo $sinopsis| awk '{ split($0,a,"\""); print a[2]}')
+                nota=$(curl "https://api.themoviedb.org/3/search/movie?api_key=8ae08e11c69497e279f94e747befdbd2&query=${titulo}&year=${year}&language=es-ES" | jq '.results[].vote_average')
+                nota_e=$(echo $nota| awk '{ split($0,a," "); print a[1]}')
+                titulo=$(echo $titulo| sed -e 's/%20/ /g')
+                TOKEN="6408734443:AAHRtxZDrKiYZFWvxPpXm1Y97EnKiPnSoGM"
+                CHAT_ID="-1002094716188"
+                MESSAGE="❌PELICULA ELIMINADA
+<b>Nota (TMDB): </b>$nota_e
+<b>Año: </b>$year
+<b>Título: </b>$titulo
+<b>Sinopsis: </b>$sinopsis_e"
+		IMAGE_PATH="$(pwd)/$portada_envio"
+                curl -X POST \
+                -H 'Content-Type: multipart/form-data' \
+                -F chat_id=$CHAT_ID \
+                -F photo=@$IMAGE_PATH \
+                -F caption="$MESSAGE" \
+                -F parse_mode="HTML" \
+                https://api.telegram.org/bot$TOKEN/sendPhoto
+        done < borradas.temp
+        rm -rf $portada_envio
+}
